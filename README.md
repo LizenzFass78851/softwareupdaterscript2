4578ea8a7cc6affb37abc317a04cb13506bb5112
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2022-02-08 16:36:49 +0100

    upload files and change readme file

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Is a script that the supported software can be updated via the task planning of 
 - then create task planning in windows (the task is best carried out as a system when the system is started)
 - In this case the script installs an update of firefox and adobe reader dc and then deletes the setups used for this.
 - The script also allows possibilities if you want to update more than just firefox and the adobe reader dc. (you have to write the parameters yourself if you want)
+
+# Out NOW - The XL variant 
+- there is now the optional additional script softwareupdaterXL.bat.
+- The difference is that the XL variant now installs more software than just firefox or the reader dc silent.
+- The silent install is supported according to the same principle as explained above: Firefox, Thunderbird, Adobe Reader DC also in the exe variant, Libreoffice, vlc, xmedia recode, furmark, ccleaner, virtualdj, Macrium Reflect, dumo, sumo
---
+++ softwareupdaterXL.bat
@@ -0,0 +1,68 @@
+@echo off
+
+pushd "%CD%"
+CD /D "%~dp0"
+
+TITLE SW-Updater-Script
+
+
+:install-firefox
+ECHO Firefox
+(for %%a IN ("firefox*.exe") DO "%%a" /s && del "%%a" )
+
+:install-thunderbird
+ECHO Thunderbird
+(for %%a IN ("thunderbird*.exe") DO "%%a" /s && del "%%a" )
+
+:install-readerdc-update-pack
+ECHO Adobe Reader DC (msp)
+(for %%a IN (acrordrdc*.msp) DO msiexec.exe /p %%a /q /norestart && del %%a )
+
+:install-readerdc-exe-setup
+ECHO Adobe Reader DC (exe)
+(for %%a IN (AcroRdrDC*.exe) DO %%a /sAll /rs /msi EULA_ACCEPT=YES && del %%a )
+
+:install-libreoffice
+ECHO LibreOffice
+(for %%a IN (LibreOffice*.msi) DO msiexec.exe /i %%a /q /norestart && del %%a )
+
+:install-vlc
+ECHO VLC
+(for %%a IN (vlc*.exe) DO %%a /S && del %%a )
+
+:install-virtualbox
+ECHO VirtualBox
+(for %%a IN (VirtualBox*.exe) DO %%a --silent && del %%a )
+REM not silent step and file after not deleted
+(for %%a IN (Oracle_VM_VirtualBox_Extension_Pack*.vbox-extpack) DO explorer %%a )
+
+:install-xmediarecode
+ECHO Xmedia Recode
+(for %%a IN (XMediaRecode*.exe) DO %%a /VERYSILENT /NORESTART && del %%a )
+
+:install-furmark
+ECHO FurMark
+(for %%a IN (FurMark*.exe) DO %%a /VERYSILENT /NORESTART && del %%a )
+
+:install-ccleaner
+ECHO ccleaner
+(for %%a IN ("ccsetup*.exe") DO "%%a" /S && del "%%a" )
+
+:install-virtualdj
+ECHO Virtualdj
+(for %%a IN (install_virtualdj*.msi) DO msiexec.exe /i %%a /q /norestart && del %%a )
+
+:install-reflect
+ECHO Macrium Reflect
+(for %%a IN (reflect_setup*.exe) DO %%a /q /norestart && del %%a )
+
+:install-dumo
+ECHO DUMO
+(for %%a IN (dumo*.exe) DO %%a /VERYSILENT /NORESTART && del %%a )
+
+:install-sumo
+ECHO SUMO
+(for %%a IN (sumo*.exe) DO %%a /VERYSILENT /NORESTART && del %%a )
+
+:exits
+exit
