2abd25a90b438307d665eff6861bf3359d7fccd5
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-12-13 15:16:44 +0100

    Update softwareupdaterX.bat
    add support for exe version of adobe reader dc

--- softwareupdaterX.bat
+++ softwareupdaterX.bat
@@ -11,6 +11,9 @@ TITLE SW-Updater-Script
 :install-readerdc-update-pack
 (for %%a IN (acrordrdc*.msp) DO echo %%a && msiexec.exe /p %%a /q /norestart && del %%a )
 
+:install-readerdc-exe-setup
+(for %%a IN (AcroRdrDC*.exe) DO echo %%a && %%a /sAll /rs /msi EULA_ACCEPT=YES && del %%a )
+
 :install-office-msi-updates
 (for %%a IN (*x-none.msp) DO echo %%a && msiexec.exe /p %%a /q /norestart && del %%a )
 
