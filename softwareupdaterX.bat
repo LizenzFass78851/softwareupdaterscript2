063b7902dbe53b3515a1533c2c14c963769d26a3
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-12-24 20:19:57 +0100

    Apply some silent install steps to SwUpdaterX Variant

--- softwareupdaterX.bat
+++ softwareupdaterX.bat
@@ -14,6 +14,18 @@ TITLE SW-Updater-Script
 :install-readerdc-exe-setup
 (for %%a IN (AcroRdrDC*.exe) DO echo %%a && %%a /sAll /rs /msi EULA_ACCEPT=YES && del %%a )
 
+:install-keepass-2
+(for %%a IN (KeePass-2*-Setup.exe) DO echo %%a && %%a /VERYSILENT /NORESTART /ALLUSERS && del %%a )
+
+REM :install-pdf24-creator
+REM (for %%a IN (pdf24-creator*.exe) DO echo %%a && %%a /VERYSILENT /NORESTART && del %%a )
+
+:install-notepadpp
+(for %%a IN ("npp.*.Installer*.exe") DO echo %%a && "%%a" /S && del "%%a" )
+
+:install-greenshot
+(for %%a IN (Greenshot-INSTALLER-*-RELEASE.exe) DO echo %%a && %%a /SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLUSERS && del %%a )
+
 :install-office-msi-updates
 (for %%a IN (*x-none.msp) DO echo %%a && msiexec.exe /p %%a /q /norestart && del %%a )
 
